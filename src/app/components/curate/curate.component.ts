34dbe6ab5986b7f8762e63b24acd40c311443853
hamzaQuantiphi <hamza.moiyadi@quantiphi.com>
2017-11-16 19:04:58 +0530

    _

--- src/app/components/curate/curate.component.ts
+++ src/app/components/curate/curate.component.ts
@@ -39,7 +39,7 @@ export class CurateComponent implements OnInit {
   selectedColumns: string
   columnSelected: boolean = false
   
-  tags : string[]
+  tags : any
 
   MIOCategories: any[] = [
     "MIO-INTERNAL_USE",
