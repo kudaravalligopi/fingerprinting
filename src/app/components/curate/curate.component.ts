93b32c7fbe78759b0086b4530304081bbc98fbb2
hamzaQuantiphi <hamza.moiyadi@quantiphi.com>
2017-11-16 19:22:33 +0530

    .

--- src/app/components/curate/curate.component.ts
+++ src/app/components/curate/curate.component.ts
@@ -102,7 +102,7 @@ export class CurateComponent implements OnInit {
     console.log(`${this.tags.MIO}`);
     
     
-    this.tags = this.makeArray(this.tags)
+    this.tags = this.makeArray(Object.keys(this.tags))
     this.getZones()
     this.createFormControls()
     this.createForm()
