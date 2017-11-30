f00d3800977a3458149fa23dabf4c00b09452f42
hamzaQuantiphi <hamza.moiyadi@quantiphi.com>
2017-11-30 18:27:56 +0530

    x

--- server/routes/fingerprint.js
+++ server/routes/fingerprint.js
@@ -35,78 +35,78 @@ router.get('/zones', (req, res) => {
 
 
 router.post('/sources', (req, res) => {
-    try{
+    try {
         var op;
         console.log(req.body.zoneName)
         let getSourcesCommand = `http://10.224.69.47:9090/v1/s3/listdatabase?bucket_name=${req.body.zoneName}`
-        exec('curl '+getSourcesCommand, (error, stdout, stderr)=>{
+        exec('curl ' + getSourcesCommand, (error, stdout, stderr) => {
             op = JSON.parse(stdout)
-            console.log(typeof(op))
+            console.log(typeof (op))
             console.log(op)
             res.json(op)//To send JSON Back
-            console.log('stderr: '+stderr)
-            if(error){
+            console.log('stderr: ' + stderr)
+            if (error) {
                 console.log(error)
             }
-    
+
         })
-    }catch(err){
+    } catch (err) {
         console.log('an error has occured')
         console.log(err)
-        res.json({"Error":err})
+        res.json({ "Error": err })
     }
-    
-    
+
+
 
 });
 
 router.post('/tables', (req, res) => {
-        
-    try{
+
+    try {
         //to get tables based on source selected
-    let getTablesCommand = `http://10.224.69.47:9090/v1/s3/listtables?bucket_name=${req.body.zoneName}&database_name=${req.body.sourceName}`
-    console.log(getTablesCommand)
-        exec('curl "' + getTablesCommand+'"',{maxBuffer:1024*1024}, (error, stdout, stderr)=>{
+        let getTablesCommand = `http://10.224.69.47:9090/v1/s3/listtables?bucket_name=${req.body.zoneName}&database_name=${req.body.sourceName}`
+        console.log(getTablesCommand)
+        exec('curl "' + getTablesCommand + '"', { maxBuffer: 1024 * 1024 }, (error, stdout, stderr) => {
             op = JSON.parse(stdout)
             console.log(op)
             res.json(op)
-            if(error !== null){
+            if (error !== null) {
                 console.log(error)
             }
         })
     }
-    catch(err){
+    catch (err) {
         console.log('an error has occured')
         console.log(err)
-        res.json({"Error":err})
+        res.json({ "Error": err })
     }
 
-    
-    
+
+
 
 });
 
 router.post('/columns', (req, res) => {
-    
+
     try {
         //     //to get columns based on table selected
-    let getColumnsCommand = `curl "http://10.224.69.47:9090/v1/s3/rawzone/listcolumns?bucket_name=${req.body.zoneName}&database_name=${req.body.sourceName}&table_name=${req.body.tableName}"`
-    console.log(getColumnsCommand)
-    
-        exec(getColumnsCommand,{maxBuffer:1024*1024}, (error, stdout, stderr)=>{
-            
+        let getColumnsCommand = `curl "http://10.224.69.47:9090/v1/s3/rawzone/listcolumns?bucket_name=${req.body.zoneName}&database_name=${req.body.sourceName}&table_name=${req.body.tableName}"`
+        console.log(getColumnsCommand)
+
+        exec(getColumnsCommand, { maxBuffer: 1024 * 1024 }, (error, stdout, stderr) => {
+
             op = JSON.parse(stdout)
             console.log(op)
-            res.json(op)        
-            if(error !== null){
+            res.json(op)
+            if (error !== null) {
                 console.log(error)
             }
         })
     }
-    catch(err){
+    catch (err) {
         console.log('an error has occured')
         console.log(err)
-        res.json({"Error":err})
+        res.json({ "Error": err })
     }
 
 
@@ -114,54 +114,50 @@ router.post('/columns', (req, res) => {
 
 router.post('/final', (req, res) => {
     console.log('IN FINGRERPRINT DATA ENDPOINT')
-    try {
-        let zone = req.body.zoneName
-        let source = req.body.sourceName
-        let table = req.body.tableName
-        let column = []
-        for(let i = 0; i< req.body.columnName.length; i++){
-            column[i] = `"${req.body.columnName[i]}"`
-        }
-    
-        let dataString =  `{"data": {"table_name": "${table}", "database_name": "${source}", "bucket_name": "${zone}", "type": "raw", "colums": [${column}]}}`
-    
-        const headers = {
-            'Content-Type':'application/json'
-        }
-        
-        console.log(dataString)    
-    
-        const options = {
-            url: 'http://10.224.69.47:9090/v1/autotagging/submit',
-            method: 'POST',
-            headers: headers,
-            body: dataString
-        };
-    
-        function callback(error, response, body) {
-            console.log('in callback')
-            if(!error){
-                console.log(body)
-                console.log('type of response is : ')
-                console.log(typeof(body))  
-                res.json(body)
-            }
-            if(error) {
-                console.log('An Error has occured')
-                console.log(error)
-                console.log('With Status Code')
-                
-            }
 
-
-            request(options, callback)
-        }    
-    } catch(err){
-        console.log('an error has occured')
-        console.log(err)
-        res.json({"Error":err})
+    let zone = req.body.zoneName
+    let source = req.body.sourceName
+    let table = req.body.tableName
+    let column = []
+    for (let i = 0; i < req.body.columnName.length; i++) {
+        column[i] = `"${req.body.columnName[i]}"`
     }
-    
+
+    let dataString = `{"data": {"table_name": "${table}", "database_name": "${source}", "bucket_name": "${zone}", "type": "raw", "colums": [${column}]}}`
+
+    const headers = {
+        'Content-Type': 'application/json'
+    }
+
+    console.log(dataString)
+
+    const options = {
+        url: 'http://10.224.69.47:9090/v1/autotagging/submit',
+        method: 'POST',
+        headers: headers,
+        body: dataString
+    };
+
+    function callback(error, response, body) {
+        console.log('in callback')
+        if (!error) {
+            console.log(body)
+            console.log('type of response is : ')
+            console.log(typeof (body))
+            res.json(body)
+        }
+        if (error) {
+            console.log('An Error has occured')
+            console.log(error)
+            console.log('With Status Code')
+
+        }
+
+
+        request(options, callback)
+    }
+
+
 })
 
 
