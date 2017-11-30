699cae5b18a688749193ec4c76ef3dcaa804b12a
Gopi Kudaravalli <kudaravalligopi@gmail.com>
2017-11-30 01:46:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 // Get dependencies
-const express = require('express');
+const express = require('express')
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport')
 const LdapStrategy = require('passport-ldapauth')
+const session = require('express-session');
+
 
 const OPTS = {
   server : {
     url:'ldap://ldapad.lmig.com:389',
+    bindDn: 'sacip_lmb_hdp_np_adm@lm.lmig.com',
+    bindCredentials: '4Ap#lz4zFbWR6#',
     searchBase: 'dc=lm,dc=lmig,dc=com',
     searchFilter:'(UserPrincipalName={{username}}*)',
-    searchScope: 'ldap3.SUBTREE',
-    searchAttributes: ['memberOf']
+    searchScope: 'ldap3.SUBTREE'
+    //searchAttributes: ['memberOf']
     //bindCredentials:'4Ap#lz4zFbWR6#'
-  },
-  usernameField:'n0304024',
-  passwordField: 'Ar$en@l4'
-
+  }
 }
 
 
@@ -27,9 +28,16 @@ const fingerprint = require('./server/routes/fingerprint');
 const login = require('./server/routes/login')
 const curate = require('./server/routes/curate')
 const app = express();
+app.use(session({
+  secret: 'ldap secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 2419200000 } /// maxAge in milliseconds
+}));
+passport.use(new LdapStrategy(OPTS));
 
-passport.use(new LdapStrategy(OPTS))
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Parsers for POST data
 app.use(bodyParser.json());
@@ -38,10 +46,23 @@ app.use(passport.initialize())
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.post('/login', passport.authenticate('ldapauth'),(req, res)=>{
-  console.log(req.body)
-  res.send({status:'ok'})
-})
+
+// app.post('/login', passport.authenticate('ldapauth'),(req, res)=>{
+//   console.log(req.body)
+//   res.send({status:'ok'})
+// })
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.post('/login', passport.authenticate('ldapauth', {
+  successRedirect: '/', failureRedirect: '/login',failureFlash: true
+})); 
 
 app.use(cors())
 // Set our api routes
