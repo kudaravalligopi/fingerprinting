d161344cc56bd5f4f491026807aa015380ff0694
hamzaQuantiphi <hamza.moiyadi@quantiphi.com>
2018-01-11 20:16:01 +0530

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {MatIconRegistry} from '@angular/material'
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import {FormLoginComponent} from './components/form-login/form-login.component'
 import {ApiService} from './services/api.service'
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,7 +15,7 @@ export class AppComponent {
   title = 'app';
 
 
-  constructor(public apiCall: ApiService, public globals: Globals, public dialog: MatDialog, matIconRegistry: MatIconRegistry, sanitizer: DomSanitizer){
+  constructor(public apiCall: ApiService, public globals: Globals, public dialog: MatDialog, matIconRegistry: MatIconRegistry, sanitizer: DomSanitizer, public router: Router){
     matIconRegistry.addSvgIcon('lmLogo', sanitizer.bypassSecurityTrustResourceUrl('assets/lmTool.svg'))
     // this.apiCall.loginCheck().subscribe(data=>{
 
@@ -24,9 +25,7 @@ export class AppComponent {
 
 
   logOut() {
-    this.apiCall.logOut().subscribe((data)=>{
-      console.log(data);
-      
-    })
+    this.router.navigate(['/logout'])
   }
+  
 }
